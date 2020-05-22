12591508fa836a3a96d166957630e64cd945af84
Kate McKinney <kate.mckinney210@gmail.com>
2020-05-22 16:57:11 -0500

    adding some starter stuff

--- src/main/java/com/gardenseedr/gardenseedr/models/User.java
+++ src/main/java/com/gardenseedr/gardenseedr/models/User.java
@@ -1,4 +1,14 @@
 package com.gardenseedr.gardenseedr.models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+//    @Column(name = "username", ) finish this later
 }
