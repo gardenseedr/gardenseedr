3695121dfc538e73eee53964594cd829d2789392
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-05-22 14:16:58 -0500

    test commit

---
+++ src/main/java/com/gardenseedr/gardenseedr/GardenseedrApplication.java
@@ -0,0 +1,13 @@
+package com.gardenseedr.gardenseedr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenseedrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenseedrApplication.class, args);
+    }
+
+}
