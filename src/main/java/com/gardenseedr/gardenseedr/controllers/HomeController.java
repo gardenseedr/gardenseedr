6420059e2fcd559d2607aeae0eee0452fb289442
Kate McKinney <kate.mckinney210@gmail.com>
2020-05-26 15:55:33 -0500

    added homecontroller stuff

--- src/main/java/com/gardenseedr/gardenseedr/controllers/HomeController.java
+++ src/main/java/com/gardenseedr/gardenseedr/controllers/HomeController.java
@@ -1,4 +1,17 @@
 package com.gardenseedr.gardenseedr.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String landing() {
+        return "index";
+    }
 }
