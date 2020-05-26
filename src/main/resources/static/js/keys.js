6ff46020de4d5aa4a72426885579cb9910514f9c
Kate McKinney <kate.mckinney210@gmail.com>
2020-05-26 18:22:39 -0500

    removed keys

--- src/main/resources/static/js/keys.js
+++
@@ -1,4 +0,0 @@
-const mapboxToken = "pk.eyJ1Ijoia2F0bWNrMTQiLCJhIjoiY2s2dHR6NzRwMDJ6czNscDBocTd3c2p3cyJ9.keGCBnHXEygmNROjAxzelw";
-const DarkSkyToken = "f1ebcb737c59f7cb6b597e45816999fe";
-const apiKeyNYT = 'sQZY872uLw5DqcINvdCSEQz8fKpUUz9U';
-const gitHubToken = "59afe25fa46de80dbd8c55ca2b9e512e744840b5";
