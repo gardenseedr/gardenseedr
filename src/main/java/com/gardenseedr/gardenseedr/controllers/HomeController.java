9b2bee8bd8dd52b7a2f33577c47cefa7ec9939fa
Kate McKinney <kate.mckinney210@gmail.com>
2020-05-26 15:56:32 -0500

    clean up

--- src/main/java/com/gardenseedr/gardenseedr/controllers/HomeController.java
+++ src/main/java/com/gardenseedr/gardenseedr/controllers/HomeController.java
@@ -2,10 +2,8 @@ package com.gardenseedr.gardenseedr.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
