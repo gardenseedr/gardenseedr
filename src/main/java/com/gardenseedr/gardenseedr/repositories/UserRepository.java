d725d7b86841b0242ad224925423ccfa398ebdcc
Andrew Ball <andy.bob.ball@gmail.com>
2020-05-27 10:28:05 -0500

    work on the registration page

--- src/main/java/com/gardenseedr/gardenseedr/repositories/UserRepository.java
+++ src/main/java/com/gardenseedr/gardenseedr/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package com.gardenseedr.gardenseedr.repositories;
 
-public interface UserRepository {
+import com.gardenseedr.gardenseedr.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
+    User findByUsername(String username);
 }
