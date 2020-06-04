501a6958442d6977cfbf0831f3f8a1ea2bfd21bf
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-06-04 15:38:05 -0500

    working on save functionality

--- src/main/resources/static/js/userGarden.js
+++ src/main/resources/static/js/userGarden.js
@@ -6,17 +6,17 @@ $(".label").click(function () {
 $(this).css("color", "white")
 });
 
-
 $(".garden-square").click(function () {
 $(".plantSearch").hide();
     var plantName = null;
     plantName = $(this).children().attr('plant-name');
     $("#modal-title").text(plantName);
     console.log(plantName);
-    if (plantName == undefined){
-        var plantSearch = "Find a plant to plant";
-        $("#modal-title").html(plantSearch);
-    }
+    /////////////commented out cause it was causing issues with the modal////////////
+    // if (plantName == undefined){
+    //     var plantSearch = "Find a plant to plant";
+    //     $("#modal-title").html(plantSearch);
+    // }
     var plantBody = null;
     plantBody = $(this).children().attr('plant-body');
     $("#modal-body").text(plantBody);
@@ -24,7 +24,6 @@ $(".plantSearch").hide();
         var plantSearch = "search";
         $(".plantSearch").show();
         $("#modal-body").text(plantSearch);
-
     }
 
 });
