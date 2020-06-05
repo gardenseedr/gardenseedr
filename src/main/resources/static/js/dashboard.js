61da55ded1f4f8c4c35ee186c959a1f28889a3e8
Kate McKinney <kate.mckinney210@gmail.com>
2020-06-05 11:56:17 -0500

    random garden tip working on dashboard

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -7,16 +7,25 @@ var gardeningTips = [
     "Garden vegetables that become over-ripe are an easy target for some pests. Remove them as soon as possible to avoid detection.",
     "Insects can’t stand plants such as garlic, onions, and chives. Grow these plants around the garden to help repel insects.",
     "Milk jugs, soda bottles and other plastic containers make great mini-covers to place over your plants and protect them from frost.",
-    "Healthy soil means healthy plants that are better able to resist pests and disease, reducing the need for harmful pesticides.",
+    "Healthy soil means healthy plants that are better able to resist pests and disease, reducing the need for pesticides.",
     "Earthworms are extremely beneficial in the vegetable garden; increasing air space in the soil and leaving behind worm castings. Do what you can to encourage earthworms in your soil.",
     "Some vegetables actually become better after a first frost, including cabbage, parsnips, carrots, and Brussels sprouts.",
     "Water your garden in the early morning to conserve moisture loss and to help avoid powdery mildew and other fungal diseases that are often spread by high humidity levels.",
     "Over watering is worse than under watering. It is easier to revive a dry plant than try to dry out drowned roots.",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
+    "A net over top of your garden is a great way to keep birds and other larger pests from bothering your plants.",
+    "Know your USDA Hardiness Zone. Use it as a guide so you don't plant trees, shrubs, and perennials that won't survive conditions in your area. You'll also get a better idea of when to plant vegetables and fruits in your area.",
+    "The best approaches to controlling weeds in the garden are hand-weeding and hoeing. Avoid deep hoeing or cultivating that can bring weed seeds to the soil's surface. Weed early and often so weeds don't go to seed. Use mulch to smother and prevent annual weeds.",
+    "The optimal temperature for ripening tomatoes is between 68-77 degrees F. And at 85 degrees F, it's too hot for the plants to produce lycopene and carotene, the pigments responsible for the fruit color. Once temperatures consistently drop below 50 degrees F, green fruits will not ripen. Tomatoes that have a bit of color change can be brought inside to finish ripening.",
+    "If your rhubarb sends up flower stalks, remove them so the plant will focus on foliage production, not seed production.",
+    "When transplanting container-grown plants, dig a hole larger than the soil ball of the plant to aid with root establishment.",
+    "Don't send your fall leaves away! Chop them up and use them as compost ingredients. Pulverized leaves can be left to nourish the lawn. After several hard freezes, when plants have gone completely dormant, you also can use 3-6 inches of shredded leaves as mulch over tender perennials to keep them dormant over winter. Remove the mulch in spring."
 ];
+
+// Get a random garden tip from the array
+function getAClue(array) {
+    var randomNum = Math.floor(Math.random() * Math.floor(array.length - 1));
+    return array[randomNum];
+}
+
+
+$("#gardeningTipsDiv").html("A quick tip: ").append(getAClue(gardeningTips));
