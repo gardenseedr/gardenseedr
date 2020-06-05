e290607f7e8e8e1aa1f7ed312cc9b95cdd1663a0
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-06-05 11:10:04 -0500

    began finding quotes

--- src/main/resources/static/js/userGarden.js
+++ src/main/resources/static/js/userGarden.js
@@ -1,117 +1,58 @@
 $(document).ready(function () {
 
+    $(".garden-square").click(function () {
+        var plantSquare = $(this).attr("id");
+        console.log(plantSquare);
+        $("#square-num").val(plantSquare);
+        $(".plantSearch").hide();
+        var plantName = null;
+        plantName = $(this).children().attr('plant-name');
+        $("#modal-title").text(plantName);
+        var plantBody = null;
+        plantBody = $(this).children().attr('plant-body');
+        $("#modal-body").text(plantBody);
+        if (plantBody == undefined) {
+            var plantSearch = "search";
+            $(".plantSearch").show();
+            $("#modal-body").text(plantSearch);
+        }
+    });
+});
 
-    $(".subheader").css("background", "blue");
 
 
-
-
-$(".garden-square").click(function () {
-    var plantSquare =  $(this).attr("id");
-    console.log(plantSquare);
-    $("#square-num").val(plantSquare);
-$(".plantSearch").hide();
-    var plantName = null;
-    plantName = $(this).children().attr('plant-name');
-    $("#modal-title").text(plantName);
-    console.log(plantName);
-    /////////////commented out cause it was causing issues with the modal////////////
-    // if (plantName == undefined){
-    //     var plantSearch = "Find a plant to plant";
-    //     $("#modal-title").html(plantSearch);
+    // /*<![CDATA[*/
+    // var plant = /*[[${allThePlants}]]*/ 'plant';
+    // /*]]>*/
+    //
+    // $('#txtSearch').on('keyup', function () {
+    //     var value = $(this).val();
+    //     var filteredData = filterFunction(value, plant);
+    //
+    //     rebuildTable(filteredData);
+    // });
+    //
+    // function filterFunction(value, data) {
+    //     var filteredData = [];
+    //     for (var i = 0; i < data.length; i++) {
+    //         var plant = data[i].toLowerCase();
+    //         if (plant.includes(value)) {
+    //             filteredData.push(data[i])
+    //         }
+    //     }
+    //     return filteredData;
     // }
-    var plantBody = null;
-    plantBody = $(this).children().attr('plant-body');
-    $("#modal-body").text(plantBody);
-    if (plantBody == undefined){
-        var plantSearch = "search";
-        $(".plantSearch").show();
-        $("#modal-body").text(plantSearch);
+    //
+    // function rebuildTable(data) {
+    //     console.log(data);
+    //     var table = document.getElementById('plantTable');
+    //     var html = '<div class="modal-body">';
+    //     data.forEach(function (plantName) {
+    //         html +=
+    //             '<span>' +
+    //             plantName +
+    //             '</span>';
+    //     });
+    //     table.innerHTML = html;
+    // }
 
-  
-    function convertDate(str) {
-        var parts = str.split("-");
-        return new Date(parts[1] + "/" + parts[2] + "/" + parts[0]);
-    }
-
-    let lastWatered = $(this).children().attr('plant-lastWatered');
-    let lastWateredConv = convertDate(lastWatered);
-    let today = $(this).children().attr('plant-today');
-    let todayDateConv = convertDate(today);
-    let dateMath = Math.floor((todayDateConv - lastWateredConv) / (24 * 3600 * 1000));
-
-    if (dateMath < 1) {
-        $(".garden-square").css("background", "blue");
-    }
-    if (dateMath < 3) {
-        $(".garden-square").css("background", "green");
-    }
-    if (dateMath > 5) {
-        $(".garden-square").css("background", "yellow");
-    }
-    if (dateMath > 10) {
-        $(".garden-square").css("background", "orange");
-    }
-    if (dateMath > 15) {
-        $(".garden-square").css("background", "red");
-
-    }
-
-
-
-
-
-
-    let lastWatered = null;
-    lastWatered = $(".plantData").attr('plant-lastWatered');
-    console.log(lastWatered);
-    let lastWateredConv = convertDate(lastWatered);
-    let today = $(".plantData").attr('plant-today');
-    let todayDateConv = convertDate(today);
-
-    let dateMath = Math.floor((todayDateConv - lastWateredConv) / (24 * 3600 * 1000));
-
-    if (lastWatered == null) {
-        $(".innerBox").hide();
-    } else {
-        if(dateMath > 15) {
-            $(".plantData").css("background", "red");
-            console.log("red? " + dateMath);
-        }
-        else if (dateMath < 10) {
-            $(".plantData").css("background", "orange");
-            console.log("orange? " + dateMath);
-        }
-        else if (dateMath < 5) {
-            $(".plantData").css("background", "yellow");
-            console.log("yellow? " + dateMath);
-        }
-        else if (dateMath < 3) {
-            $(".plantData").css("background", "green");
-            console.log("green? " + dateMath);
-        }
-        else {
-            $(".plantData").css("background", "blue");
-            console.log("blue? " + dateMath);
-        }
-
-    }
-
-    function convertDate(str) {
-        var parts = str.split("-");
-        return new Date(parts[1] + "/" + parts[2] + "/" + parts[0]);
-    }
-
-})
-;
-
-
-
-
-
-
-// .text("We are well watered!")
-// .text("We're getting thirsty! It's been " + dateMath + " days")
-// .text("Don't forget to water us! It's been " + dateMath + " days")
-// .text("Help! We Need Water! It's been " + dateMath + " days")
-// .text("Oh no! Did you forget us? It's been " + dateMath + " days")
