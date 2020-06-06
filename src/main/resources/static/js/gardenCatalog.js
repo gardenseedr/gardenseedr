b961ea6dec4066927a6bbcdb651e3b5ffca9a306
Jacqueline Muralles <jacquelinenmuralles@gmail.com>
2020-06-06 13:56:28 -0500

    completed api calls for catalog

--- src/main/resources/static/js/gardenCatalog.js
+++ src/main/resources/static/js/gardenCatalog.js
@@ -1,7 +1,7 @@
 
 
 
-function fetchPlants(){
+function fetchArugula(){
     fetch("https://openfarm.cc/api/v1/crops/550144506665350003000000")
         .then(response => {
             if(!response.ok){
@@ -22,12 +22,14 @@ function fetchPlants(){
             html_var += '</div>';
             console.log(info.description);
             console.log(html_var);
-            document.getElementById('swipe').innerHTML = html_var;
+            document.getElementById('1').innerHTML = html_var;
             });
 }
 
-function fetchApiId(){
-    fetch("https://openfarm.cc//api/v1/users/5ed9760c9364840004d3872f/gardens/")
+fetchArugula();
+
+function fetchAsparagus(){
+    fetch("https://openfarm.cc/api/v1/crops/54afdda33166630002400600")
         .then(response => {
             if(!response.ok){
                 throw Error("ERROR");
@@ -35,20 +37,26 @@ function fetchApiId(){
             return response.json();
         })
         .then(data => {
-            console.log(data);
-            console.log(data.data[0].relationships.garden_crops.data);
-        })
+            console.log(data.data);
+            var picture = data.data.attributes.main_image_path;
+            console.log(picture);
+            var info = data.data.attributes;
+            var html_var = '<div>';
+            html_var += `<img class="card-img-top" id="asparagus" src="${picture}" alt="Card image cap"`;
+            html_var += '<p>' + info.name + '</p>';
+            html_var += '<p>' + info.binomial_name + '</p>';
+            html_var += '<p>' + info.description + '</p>';
+            html_var += '</div>';
+            console.log(info.description);
+            console.log(html_var);
+            document.getElementById('2').innerHTML = html_var;
+        });
 }
 
-function practiceCall() {
-    const options = {
-        method: "POST",
-        "data": {
-            "email": "jaque3985@gmail",
-            "password": "jack3985"
-        }
-    };
-    fetch("https://openfarm.cc//api/v1/gardens/5ed979ed9364840004d38733/")
+fetchAsparagus();
+
+function fetchBasil(){
+    fetch("https://openfarm.cc/api/v1/crops/543c216c3938620002020000")
         .then(response => {
             if(!response.ok){
                 throw Error("ERROR");
@@ -56,58 +64,221 @@ function practiceCall() {
             return response.json();
         })
         .then(data => {
-            console.log(data);
+            console.log(data.data);
+            var picture = data.data.attributes.main_image_path;
+            console.log(picture);
+            var info = data.data.attributes;
+            var html_var = '<div>';
+            html_var += `<img class="card-img-top" id="basil" src="${picture}" alt="Card image cap"`;
+            html_var += '<p>' + info.name + '</p>';
+            html_var += '<p>' + info.binomial_name + '</p>';
+            html_var += '<p>' + info.description + '</p>';
+            html_var += '</div>';
+            console.log(info.description);
+            console.log(html_var);
+            document.getElementById('3').innerHTML = html_var;
+        });
+}
+
+fetchBasil();
+
+
+function fetchBellPepper(){
+    fetch("https://openfarm.cc/api/v1/crops/59b0a4ac4dd0a1000400003b")
+        .then(response => {
+            if(!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
         })
-
+        .then(data => {
+            console.log(data.data);
+            var picture = data.data.attributes.main_image_path;
+            console.log(picture);
+            var info = data.data.attributes;
+            var html_var = '<div>';
+            html_var += `<img class="card-img-top" id="bellPepper" src="${picture}" alt="Card image cap"`;
+            html_var += '<p>' + info.name + '</p>';
+            html_var += '<p>' + info.binomial_name + '</p>';
+            html_var += '<p>' + info.description + '</p>';
+            html_var += '</div>';
+            console.log(info.description);
+            console.log(html_var);
+            document.getElementById('4').innerHTML = html_var;
+        });
 }
 
-// practiceCall();
-//
-// fetchApiId();
+fetchBellPepper();
 
 
-fetchPlants();
-
-// function brancsCrazyIdea() {
-//     const options = {
-//         method: "GET",
-//         "data": {
-//             "email": "jaque3985@gmail",
-//             "password": "jack3985"
-//         }
-//     };
-//     fetch('https://openfarm.cc//api/v1/token/', options)
-//         .then(response => {
-//             if(!response.ok){
-//                 throw Error("ERROR");
-//             }
-//             return response.json();
-//         })
-//         .then(data => {
-//             console.log(data);
-//         })
-// }
-//
-// brancsCrazyIdea();
-
-function ajaxcall() {
-    const options = {
-        method: "POST",
-        data: {
-            email: "jaque3985@gmail",
-            password: "jack3985"
-        }
-    };
-    $.ajax("https://openfarm.cc//api/v1/token/").done(function (data) {
-        console.log(data);
-    }).fail(function (param, thing, hello) {
-        console.log(param);
-        console.log(thing);
-        console.log(hello);
-    })
+function fetchBroccoli(){
+    fetch("https://openfarm.cc/api/v1/crops/54afdefe3166630002990900")
+        .then(response => {
+            if(!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.data);
+            var picture = data.data.attributes.main_image_path;
+            console.log(picture);
+            var info = data.data.attributes;
+            var html_var = '<div>';
+            html_var += `<img class="card-img-top" id="broccoli" src="${picture}" alt="Card image cap"`;
+            html_var += '<p>' + info.name + '</p>';
+            html_var += '<p>' + info.binomial_name + '</p>';
+            html_var += '<p>' + info.description + '</p>';
+            html_var += '</div>';
+            console.log(info.description);
+            console.log(html_var);
+            document.getElementById('5').innerHTML = html_var;
+        });
 }
 
-// ajaxcall();
+fetchBroccoli();
+
+
+function fetchBrusselsSprout(){
+    fetch("https://openfarm.cc/api/v1/crops/54afdf033166630002ae0900")
+        .then(response => {
+            if(!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.data);
+            var picture = data.data.attributes.main_image_path;
+            console.log(picture);
+            var info = data.data.attributes;
+            var html_var = '<div>';
+            html_var += `<img class="card-img-top" id="brusselsSprout" src="${picture}" alt="Card image cap"`;
+            html_var += '<p>' + info.name + '</p>';
+            html_var += '<p>' + info.binomial_name + '</p>';
+            html_var += '<p>' + info.description + '</p>';
+            html_var += '</div>';
+            console.log(info.description);
+            console.log(html_var);
+            document.getElementById('6').innerHTML = html_var;
+        });
+}
+
+fetchBrusselsSprout();
+
+
+function fetchCabbage(){
+    fetch("https://openfarm.cc/api/v1/crops/542e9dce63313600020f1300")
+        .then(response => {
+            if(!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.data);
+            var picture = data.data.attributes.main_image_path;
+            console.log(picture);
+            var info = data.data.attributes;
+            var html_var = '<div>';
+            html_var += `<img class="card-img-top" id="cabbage" src="${picture}" alt="Card image cap"`;
+            html_var += '<p>' + info.name + '</p>';
+            html_var += '<p>' + info.binomial_name + '</p>';
+            html_var += '<p>' + info.description + '</p>';
+            html_var += '</div>';
+            console.log(info.description);
+            console.log(html_var);
+            document.getElementById('7').innerHTML = html_var;
+        });
+}
+
+fetchCabbage();
+
+
+function fetchCarrot(){
+    fetch("https://openfarm.cc/api/v1/crops/551ddaf53938340003580000")
+        .then(response => {
+            if(!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.data);
+            var picture = data.data.attributes.main_image_path;
+            console.log(picture);
+            var info = data.data.attributes;
+            var html_var = '<div>';
+            html_var += `<img class="card-img-top" id="carrot" src="${picture}" alt="Card image cap"`;
+            html_var += '<p>' + info.name + '</p>';
+            html_var += '<p>' + info.binomial_name + '</p>';
+            html_var += '<p>' + info.description + '</p>';
+            html_var += '</div>';
+            console.log(info.description);
+            console.log(html_var);
+            document.getElementById('8').innerHTML = html_var;
+        });
+}
+
+fetchCarrot();
+
+
+function fetchCelery(){
+    fetch("https://openfarm.cc/api/v1/crops/54afdd0531666300022a0400")
+        .then(response => {
+            if(!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.data);
+            var picture = data.data.attributes.main_image_path;
+            console.log(picture);
+            var info = data.data.attributes;
+            var html_var = '<div>';
+            html_var += `<img class="card-img-top" id="celery" src="${picture}" alt="Card image cap"`;
+            html_var += '<p>' + info.name + '</p>';
+            html_var += '<p>' + info.binomial_name + '</p>';
+            html_var += '<p>' + info.description + '</p>';
+            html_var += '</div>';
+            console.log(info.description);
+            console.log(html_var);
+            document.getElementById('9').innerHTML = html_var;
+        });
+}
+
+fetchCelery();
+
+function fetchChives(){
+    fetch("https://openfarm.cc/api/v1/crops/54afe3793166630002951100")
+        .then(response => {
+            if(!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.data);
+            var picture = data.data.attributes.main_image_path;
+            console.log(picture);
+            var info = data.data.attributes;
+            var html_var = '<div>';
+            html_var += `<img class="card-img-top" id="chives" src="${picture}" alt="Card image cap"`;
+            html_var += '<p>' + info.name + '</p>';
+            html_var += '<p>' + info.binomial_name + '</p>';
+            html_var += '<p>' + info.description + '</p>';
+            html_var += '</div>';
+            console.log(info.description);
+            console.log(html_var);
+            document.getElementById('10').innerHTML = html_var;
+        });
+}
+
+fetchChives();
+
+
+
 
 
 
