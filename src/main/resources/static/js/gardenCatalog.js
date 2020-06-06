ecb0898cd9d197eb94db0ce9f803a56150d09a88
Jacqueline Muralles <jacquelinenmuralles@gmail.com>
2020-06-06 12:27:27 -0500

    tried different calls to api to find a way to loop through information

--- src/main/resources/static/js/gardenCatalog.js
+++ src/main/resources/static/js/gardenCatalog.js
@@ -15,7 +15,7 @@ function fetchPlants(){
             console.log(picture);
             var info = data.data.attributes;
             var html_var = '<div>';
-            html_var += `<img class="card-img-top" id="plant-pic" src="${picture}" alt="Card image cap"`;
+            html_var += `<img class="card-img-top" id="arugula" src="${picture}" alt="Card image cap"`;
             html_var += '<p>' + info.name + '</p>';
             html_var += '<p>' + info.binomial_name + '</p>';
             html_var += '<p>' + info.description + '</p>';
@@ -24,12 +24,90 @@ function fetchPlants(){
             console.log(html_var);
             document.getElementById('swipe').innerHTML = html_var;
             });
+}
+
+function fetchApiId(){
+    fetch("https://openfarm.cc//api/v1/users/5ed9760c9364840004d3872f/gardens/")
+        .then(response => {
+            if(!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            console.log(data.data[0].relationships.garden_crops.data);
+        })
+}
+
+function practiceCall() {
+    const options = {
+        method: "POST",
+        "data": {
+            "email": "jaque3985@gmail",
+            "password": "jack3985"
         }
+    };
+    fetch("https://openfarm.cc//api/v1/gardens/5ed979ed9364840004d38733/")
+        .then(response => {
+            if(!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+
+}
+
+// practiceCall();
+//
+// fetchApiId();
 
 
 fetchPlants();
 
+// function brancsCrazyIdea() {
+//     const options = {
+//         method: "GET",
+//         "data": {
+//             "email": "jaque3985@gmail",
+//             "password": "jack3985"
+//         }
+//     };
+//     fetch('https://openfarm.cc//api/v1/token/', options)
+//         .then(response => {
+//             if(!response.ok){
+//                 throw Error("ERROR");
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log(data);
+//         })
+// }
+//
+// brancsCrazyIdea();
 
+function ajaxcall() {
+    const options = {
+        method: "POST",
+        data: {
+            email: "jaque3985@gmail",
+            password: "jack3985"
+        }
+    };
+    $.ajax("https://openfarm.cc//api/v1/token/").done(function (data) {
+        console.log(data);
+    }).fail(function (param, thing, hello) {
+        console.log(param);
+        console.log(thing);
+        console.log(hello);
+    })
+}
+
+// ajaxcall();
 
 
 
