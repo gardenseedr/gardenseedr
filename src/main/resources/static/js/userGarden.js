9a5a48d0c94da05befe951479ffb4d8ef5a6b57c
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-06-07 11:35:30 -0500

    added api call for already established crops

--- src/main/resources/static/js/userGarden.js
+++ src/main/resources/static/js/userGarden.js
@@ -7,19 +7,36 @@ $(document).ready(function () {
         console.log(plantSquare);
         $("#square-num").val(plantSquare);
         var plantName = $(this).children().attr('plant-name');
+        var apiId = $(this).children().attr('plant-body');
+        console.log(apiId);
         if (plantName == null) {
             console.log("no plant name");
             $(".plantSearch").show();
+            $("#submit-crop").show();
         } else {
+            $("#submit-crop").hide();
             $(".plantSearch").hide();
             console.log("Plant Name:" + plantName);
             $("#modal-title").text(plantName);
-            var plantBody = null;
-            plantBody = $(this).children().attr('plant-body');
+
+            // plantBody = $(this).children().attr('plant-body');
             var plantedSquareId = $(this).children().attr('plant-id');
             console.log(plantedSquareId);
-            plantBody = '<a href="/square/' + plantedSquareId + '">See more about your ' + plantName.toLowerCase() + '</a>';
-            $("#api-results").html(plantBody);
+            var plantUrl = "https://openfarm.cc/api/v1/crops/" + apiId;
+            fetch(plantUrl)
+                .then(response => response.json())
+                .then(data => {
+                    var plantBody = "";
+                    var crop = data.data.attributes;
+                    plantBody += "<p><strong>Sun Requirements: </strong>" + crop.sun_requirements + "</p>";
+                    plantBody += "<p><strong>Sowing Method: </strong>" + crop.sowing_method + "</p>";
+                    plantBody += "<p><strong>Spread: </strong>" + (((crop.spread) / 2.54).toFixed(0)) + " inches</p>";
+                    plantBody += "<p><strong>Row Spacing: </strong>" + (((crop.row_spacing) / 2.54).toFixed(0)) + " inches</p>";
+                    plantBody += "<p><strong>Height: </strong>" + (((crop.height) / 2.54).toFixed(0)) + " inches</p>";
+                    plantBody += '<a href="/square/' + plantedSquareId + '">See more about your ' + plantName.toLowerCase() + '</a>';
+                    $("#api-results").html(plantBody);
+                    console.log(crop);});
+
         }
 
         // if (plantBody == undefined) {
